dbb4362e322ba23c28b5a9f7daf468778d90ea9f
Fadis-0 <ayadfadi10@gmail.com>
2023-09-24 18:03:07 +0100

    second

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * _strdup - function that returns a pointer to a newly allocated space in memory
+ * _strdup - function that returns a pointer
  * @str: character string
  * Return: 0
  */
